2283fbe18682f27999bd1eeefa254337575b8142
Daena450 <daenaduenas6@gmail.com>
2024-06-26 21:57:54 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero porfavor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario === numeroSecreto){
+    alert ("acertastes el numero");
+}
+else{
+    alert("fallaste")
+}
